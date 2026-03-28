5769d9973ebbc1ebfea5da68b570e420b5bd8add
Egor-source <ego_rk@mail.ru>
2026-03-28 13:49:32 +0400

    [inputs]: Added re-export of core package types

--- packages/inputs/CHANGELOG.md
+++ packages/inputs/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @hotora/inputs
 
+## 1.0.2
+
+### Patch Changes
+
+- Added re-export of core package types
+
 ## 1.0.1
 
 ### Patch Changes
--- packages/inputs/src/index.ts
+++ packages/inputs/src/index.ts
@@ -2,3 +2,14 @@ export * from "./InputsManager";
 export * from "./eventProviders/DOMKeyboardEventProvider";
 export * from "./eventProviders/LazyEventProvider";
 export * from "./types";
+export type {
+  Step,
+  Stage,
+  Sequence,
+  ToString,
+  ActionId,
+  SequenceEvent,
+  Fired,
+  SequenceActionHandler,
+  SequenceAction,
+} from "@hotora/core";
